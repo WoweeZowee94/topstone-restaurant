c987a0b8d6bf475ed7234282c5b11f2490b4a1c4
Forrest Gray <forrestg@Forrests-MacBook-Pro.local>
2022-03-14 13:52:27 -0400

    finishing touches

--- client/src/components/AboutUs.js
+++ client/src/components/AboutUs.js
@@ -2,7 +2,15 @@ import React from 'react'
 
 const AboutUs = () => {
 return (
-  <h1>Hell from About Us</h1>
+  <div>
+  <h1>Made in South Windsor for South Windsor.</h1>
+  <p className='about-us-body'>
+    Three Seasons View, located on the scenic Topstone Golf Course in South Windsor CT has been delighting the people of Northeastern Connecticut for over 20 years. <br/>
+    Whether you prefer our Grill Room sporting scenic views and a rustic gastropub setting, or our Banquet Halls that are equipped to host corporate events and intimate gatherings of all sizes- Our team would love to serve you. <br/>
+    Be sure to consider Three Seasons View for your next date night, to-go dinner, family function, or corporate outing. 
+  </p>
+  <h2> Our Team </h2>
+  </div>
 )
 }
 
